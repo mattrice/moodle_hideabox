eab76e65382ea8d7e5fc9e8629c61a3a79d04644
mattrice <mrice1@midmich.edu>
2013-08-23 16:23:57 -0400

    Update version.php
    Correct required version to Moodle 2.4 (2012120300)

--- version.php
+++ version.php
@@ -5,6 +5,6 @@ defined('MOODLE_INTERNAL') || die();
 // The current plugin version (Date: YYYYMMDDXX).
 $plugin->version   = 2013082304;
 // Required Moodle version.
-$plugin->requires  = 2013051400;
+$plugin->requires  = 2012120300;
 // Full name of the plugin (used for diagnostics).
 $plugin->component = 'tinymce_hideabox';
